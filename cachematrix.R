a43e41ff833d27ffc8272d890599c5449edb7e21
Nicole Peltier <npeltier@u.rochester.edu>
2020-08-24 19:22:31 -0400

    Filled in function makeCacheMatrix to make list with functions to set/get value of matrix and to set/get inverse of matrix. Filled in function cacheSolve to compute inverse of matrix or return cached inverse if it has already been computed. Documented code with comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+### cachematrix.R ###
+### Nicole Peltier ###
+### August 24, 2020 ###
 
-## Write a short comment describing this function
+### Calculates and caches inverse of matrix to save time on
+###   repeated calls for inverse
+### First, initialize on matrix x: x_matrix <- makeCacheMatrix(x)
+### Next, get inverse of matrix: cacheSolve(x_matrix)
+### Repeated calls of cacheSolve will use cached value of inverse
 
+### makeCacheMatrix ###
+# Input: matrix x
+# Caches matrix inverse to save computation time by not having
+#   to recalculate every time it is called
+# Creates set/get functions for matrix and inverse
 makeCacheMatrix <- function(x = matrix()) {
+  # Initialize inverse to be NULL
+  inverse <<- NULL
+  
+  # Set/get functions for x
+  set <- function(y){
+    x <<- y
+    # Every time x is changed, reset inverse to NULL
+    inverse <<- NULL
+  }
+  get <- function() x
+  
+  # Set and get inverse of matrix
+  set_inverse <- function(inv) inverse <<- inv
+  get_inverse <- function() inverse
+  
+  # Return list that contain set/get functions for x and inverse
+  list(set = set,
+       get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 
 }
 
-
-## Write a short comment describing this function
-
+### cacheSolve ###
+# Input: matrix x
+# Returns matrix that is inverse of x
+# If inverse already solved/cached, returns cached inverse
+# If no cached value saved, calculates inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # First, look for value already set
+  inverse <- x$get_inverse()
+  
+  # If inverse already cached, return cached value
+  if(!is.null(inverse)){
+    message("Already calculated! Getting cached data")
+    return(inverse)
+  }
+  
+  # If inverse not cached, calculate
+  message("Not calculated yet... calculating now")
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$set_inverse(inverse)
+  
+  # Return inverse
+  inverse
 }
